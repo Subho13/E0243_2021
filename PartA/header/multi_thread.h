7b7f6a8376af54b473861c2d4d872da935572707
Subhojyoti Khastagir <skhastagir98@gmail.com>
2022-10-16 10:29:52 +0530

    Implement cpu optimizations

--- PartA/header/multi_thread.h
+++ PartA/header/multi_thread.h
@@ -1,10 +1,58 @@
 #include <pthread.h>
 
-// Create other necessary functions here
+#define NUM_THREADS 2
+typedef struct {
+    int start, end, N;
+    int *matA, *matB, *matC;
+} Args;
 
+void *cmmThread(void* param) {
+    Args* args = (Args*) param;
 
-// Fill in this function
+    for (int rowA = 0; rowA < args->N; ++rowA) {
+        int evenRowA = rowA & 1;
+        int tempIndexC = ((rowA >> 1) * (args->N)) + ((args->N) >> 1) * evenRowA;
+
+        for (int i = args->start; i < args->end; ++i) {
+            args->matC[tempIndexC + i] = 0;
+        }
+
+        for (int rowB = 0; rowB < args->N; ++rowB) {
+            int indexA = rowA * (args->N) + (rowB ^ 1);
+            int tempIndexB = rowB * (args->N);
+            int tempColB = evenRowA ^ (rowB & 1);
+
+            for (int colSetB = args->start; colSetB < args->end; ++colSetB) {
+                int colB = (colSetB << 1) | tempColB;
+                args->matC[tempIndexC + colSetB] += (args->matA[indexA]) * (args->matB[tempIndexB + colB]);
+            }
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
 void multiThread(int N, int *matA, int *matB, int *output)
 {
-    
+    int columnSets = N >> 1;
+    int colSetsPerThread = columnSets / NUM_THREADS;
+    pthread_t threads[NUM_THREADS];
+    Args args [NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        args[i].start = colSetsPerThread * i;
+        args[i].end = args[i].start + colSetsPerThread;
+        args[i].N = N;
+        args[i].matA = matA;
+        args[i].matB = matB;
+        args[i].matC = output;
+    }
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        pthread_create(&threads[i], NULL, cmmThread, (void*)(args + i));
+    }
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        pthread_join(threads[i], NULL);
+    }
 }
--- PartA/header/single_thread.h
+++ PartA/header/single_thread.h
@@ -1,50 +1,25 @@
-// Optimize this function
-
 void singleThread(int N, int *matA, int *matB, int *output)
 {
     // enforce N to be power of 2 and greater than 2
     assert(N>=4 and N == ( N &~ (N-1)));
-    for(int rowA = 0; rowA < N; rowA++){
-        for(int colSetB = 0; colSetB < N; colSetB += 2){
-            int sum = 0;
-            if(rowA & 1){    
-                // handle odd rows in matA
-                
-                // iterate over even positions in rowA
-                // and odd positions in column colSetB in matB
-                for(int indexA = rowA*N, indexB = colSetB+N; indexA < (rowA+1)*N; 
-                        indexA += 2, indexB +=(N<<1))
-                    sum += matA[indexA] * matB[indexB];
-                
-                // iterate over odd positions in rowA
-                // and even positions in column colSetB+1 in matB
-                for(int indexA = rowA*N+1, indexB = colSetB+1; indexA < (rowA+1)*N; 
-                        indexA += 2, indexB += (N<<1))
-                    sum += matA[indexA] * matB[indexB];
-            
-            } else {
-                // handle even rows in matA
+    for (int rowA = 0; rowA < N; rowA++) {
+    	int rowC = rowA >> 1;
+    	int tempIndexA = rowA * N;
+    	int tempIndexC = rowC * N + (N >> 1) * (rowA & 1);
 
-                // iterate over even positions in rowA
-                // and odd positions in column colSetB+1 in matB
-                for(int indexA = rowA*N, indexB = colSetB+1+N; indexA < (rowA+1)*N; 
-                        indexA += 2, indexB += (N<<1))
-                    sum += matA[indexA] * matB[indexB];
+		// Clear output matrix
+		for (int i = 0; i < (N>>1); ++i) {
+			output[tempIndexC + i] = 0;
+		}
+    	for (int rowB = 0; rowB < N; rowB++) {
+			int colA = rowB ^ 1;
+			int colB = (rowA + rowB) & 1;
+			int indexA = tempIndexA + colA;
 
-                // iterate over odd positions in rowA
-                // and even positions in column colSetB in matB
-                for(int indexA = rowA*N+1, indexB = colSetB; indexA < (rowA+1)*N; 
-                        indexA += 2, indexB += (N<<1))
-                    sum += matA[indexA] * matB[indexB];
-            }
-            
-            // compute output indices
-            int rowC = rowA>>1;
-            int colC = (colSetB>>1) + (N>>1) * (rowA&1);
-            int indexC = rowC * N + colC;
-            output[indexC] = sum;
-        }
+			for (int i = colB; i < N; i+=2) {
+				int indexB = rowB * N + i;
+				output[tempIndexC + (i >> 1)] += matA[indexA] * matB[indexB];
+			}
+    	}
     }
 }
-
-
